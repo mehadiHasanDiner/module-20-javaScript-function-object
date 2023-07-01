50d6e8fb9f9b14cce922faf2b28e1509983b2d25
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-02 01:00:28 +0600

    substring & includes in JS

--- include.js
+++ include.js
@@ -1,1 +1,7 @@
-var name = "ariana grande is singing";
+var myName = "Ariana Grande is singing";
+// myName.includes("is");
+
+// console.log(myName.includes("is"));
+
+console.log(myName.substring(5, 9));
+console.log(myName.includes("is"));
