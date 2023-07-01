57a4f611b783dd2e6474a71dca9158230c9e5908
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-02 01:29:40 +0600

    function.js file created

---
+++ function.js
@@ -0,0 +1,10 @@
+function startFan() {
+  console.log("stand up");
+  console.log("go towards the switch...");
+  console.log("click the switch");
+}
+
+startFan();
+console.log("watching cricket match...");
+console.log("go to washroom...");
+startFan();
