25aa5549b357e27e37b5599bacfd7cdb5bc10210
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-02 02:27:43 +0600

    parametter details

--- parameter.js
+++ parameter.js
@@ -1,6 +1,26 @@
 function bringSingara(money) {
-  console.log("mama singara den");
+  console.log("taka disen: ", money);
+  console.log("ai nen singara");
   console.log(money);
+  //   প্যারামিটার money সব সময় ফাংশনের ভিতরই একসেসএবল। ফাংশনের বাইরে ব্যবহৃত হয় না, করলে ইরর আসে
 }
 
-bringSingara(250);
+// bringSingara(250);
+// // console.log(money);
+
+// var taka = 250;
+// bringSingara(taka);
+
+function add(num1, num2) {
+  console.log("going to add:", num1, num2);
+}
+
+// add(123, 340);
+
+function sum(a, b, c, d, e) {
+  console.log("sum:", a, b, c, d, e);
+  var sum = a + b + c + d + e;
+  console.log(sum);
+}
+
+sum(10, 20, 30, 40, 50);
