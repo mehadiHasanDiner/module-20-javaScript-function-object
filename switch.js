2298235e542595a903b6e7e1e39e3a08a63b055b
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-02 19:31:44 +0600

    switch.js

---
+++ switch.js
@@ -0,0 +1,32 @@
+const color = "yellow";
+if (color === "green") {
+  console.log("You are a green color friend");
+} else if (color === "blue") {
+  console.log("You are a blue color friend");
+} else if (color === "red") {
+  console.log("You are a red color friend");
+} else if (color === "white") {
+  console.log("You are a white color friend");
+} else if (color === "yellow") {
+  console.log("You are a yellow color friend");
+} else {
+  console.log("You are a black color friend");
+}
+
+// switch
+switch (color) {
+  case "blue":
+    console.log("You are a blue color friend");
+    break;
+  case "white":
+    console.log("You are a white color friend");
+    break;
+  case "white":
+    console.log("You are white color friend");
+    break;
+  case "yellow":
+    console.log("You are a yellow color friend");
+    break;
+  default:
+    console.log("You are a kala kala friend");
+}
