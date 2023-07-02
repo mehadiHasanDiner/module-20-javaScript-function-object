d9ca9e6ff7f671b580699df0863a022dc7837372
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-02 21:48:05 +0600

    practice problem 1, 2

---
+++ practice-problem2.js
@@ -0,0 +1,15 @@
+// practice problem 2
+function make_avg(a, b, c, d) {
+  var average = (a + b + c) / 3;
+  return average;
+}
+
+var markSubject1 = 59;
+var markSubject2 = 69;
+var markSubject3 = 85;
+
+var totalAverage = make_avg(markSubject1, markSubject2, markSubject3);
+
+// var totalAverage = make_avg(89, 75, 68);
+
+console.log(totalAverage);
