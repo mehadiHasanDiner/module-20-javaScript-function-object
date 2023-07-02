74d1fb6521b1b7668fff25ee59d7090fa367f54e
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-02 16:45:31 +0600

    object.js

---
+++ object.js
@@ -0,0 +1,25 @@
+var student = {
+  id: 1112,
+  name: "Solaiman Khan",
+  class: 9,
+  marks: 98,
+};
+
+var mobile = {
+  brand: "samsung",
+  price: 19900,
+  storage: "64GB",
+  camera: "48MP",
+};
+
+var myComputer = {
+  brand: "Lenovo",
+  price: 40000,
+  color: "black",
+  processor: "i7",
+};
+
+console.log(myComputer);
+myComputer.processor = "i797";
+
+console.log(myComputer);
