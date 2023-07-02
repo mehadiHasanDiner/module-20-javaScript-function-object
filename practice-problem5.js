0d48f6e5f636de4296ca035c22727ef9cddabdd0
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-07-02 23:44:06 +0600

    practice problem 5

---
+++ practice-problem5.js
@@ -0,0 +1,30 @@
+// // if else
+
+// var signal = "green";
+
+// if (signal === "red") {
+//   console.log("it is so dangerous! I will face accident");
+// } else if (signal === "yellow") {
+//   console.log("it is not so dangerous! I have to stop crossing the road");
+// } else if (signal === "green") {
+//   console.log("it is ok! I will cross the road");
+// } else {
+//   console.log("it will walk beside the footpath");
+// }
+
+// switch
+
+var signal = "red";
+switch (signal) {
+  case "red":
+    console.log("it is so dangerous! I will face accident");
+    break;
+  case "yellow":
+    console.log("it is not so dangerous! I have to stop cross the road");
+    break;
+  case "green":
+    console.log("it is ok! I will cross the road");
+    break;
+  default:
+    console.log("it will walk beside the footpath");
+}
